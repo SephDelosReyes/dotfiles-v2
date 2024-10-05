58c85d04430889184b4d462cd01b39a5c6b33346
Seph Delos Reyes <SephDelosReyes@github.com>
2024-10-05 22:03:27 +0800

    copilot defaults

--- nvim/.config/nvim/lua/sephdr/lazy/cmp.lua
+++ nvim/.config/nvim/lua/sephdr/lazy/cmp.lua
@@ -83,10 +83,11 @@ return { -- Autocompletion
         end, { 'i', 's' }),
       },
       sources = {
-        { name = 'nvim_lsp' },
-        { name = 'luasnip' },
-        { name = 'path' },
-        { name = 'gopls' },
+        { name = 'copilot', group_index = 2 },
+        { name = 'nvim_lsp', group_index = 2 },
+        { name = 'luasnip', group_index = 2 },
+        { name = 'path', group_index = 2 },
+        -- { name = 'gopls' },
       },
     }
   end,
